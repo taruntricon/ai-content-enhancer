3eb8bd19c1c28964b153e10eb13c17ed101d073b
Harshit Jain <Harshit.Jain@triconinfotech.com>
2026-07-17 15:44:18 +0530

    added database file

---
+++ app/database/mongodb.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+from app.config import MONGODB_URI
+
+client = MongoClient(MONGODB_URI)
+
+db = client["social_media_db"]
+
+posts_collection = db["posts"]
+engagements_collection = db["engagements"]
+leads_collection = db["leads"]
+
+# post = {
+#     "postId": "7482344406616842241",
+#     "postText": "Excited to be exploring new ideas in tech and connecting with like-minded people here on LinkedIn.",
+#     "platform": "LinkedIn"
+# }
+
+# posts_collection.insert_one(post)
