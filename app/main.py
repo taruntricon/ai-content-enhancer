24bb67a83c131db83257b318a34f6ca3f3ce1238
Harshit Jain <Harshit.Jain@triconinfotech.com>
2026-07-20 22:22:57 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from app.routes.fetch_post_detail import router as post_detail_router
 from app.routes.fetch_comment_detail import router as comment_detail_router
 from app.routes.mongodb import router as mongodb_router
 from app.routes.post_analytics import router as post_analytics_router
+from app.routes.get_leads import router as get_leads_router
 
 from app.scheduler.scheduler import (
     start_scheduler,
@@ -53,6 +54,7 @@ app.include_router(post_detail_router)
 app.include_router(comment_detail_router)
 app.include_router(mongodb_router)
 app.include_router(post_analytics_router)
+app.include_router(get_leads_router)
 
 @app.get("/")
 def health():
